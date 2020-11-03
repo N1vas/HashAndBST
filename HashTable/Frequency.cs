41bdb51c5092e38b54da0dacf0fed68ae8b6cae8
N1vas <ggj.nivas.18@gmail.com>
2020-11-03 17:48:18 +0530

    [N1vas] Add : Frequency of words in a sentence can be found

---
+++ HashTable/Frequency.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+namespace HashTable
+{
+    class Frequency
+    {
+        Dictionary<string, int> frequency = new Dictionary<string, int>();
+        public void FrequencyData(MyMapNode<int,string> data)
+        {
+            for (int key = 0; key < data.size; key++)
+            {
+                frequency.TryAdd(data.Get(key).ToLower(), 0);
+                frequency[data.Get(key).ToLower()]++;
+            }
+            foreach (KeyValuePair<string, int> item in frequency)
+            {
+                Console.WriteLine($"frequency of word '{item.Key}' is {item.Value}");
+            }
+        }
+    }
+}
---
+++ HashTable/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace HashTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyMapNode<int, string> hash = new MyMapNode<int, string>(6);
+            hash.Add(0, "To");
+            hash.Add(1, "be");
+            hash.Add(2, "or");
+            hash.Add(3, "not");
+            hash.Add(4, "to");
+            hash.Add(5, "be");
+            Frequency count = new Frequency();
+            count.FrequencyData(hash);
+        }
+    }
+}
