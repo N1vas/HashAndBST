7b095432535337ee7e66e1b76303d3c590c6078f
N1vas <ggj.nivas.18@gmail.com>
2020-11-03 17:58:47 +0530

    [N1vas] Refactor : Frequency of words in paragraph can be found

--- HashTable/Program.cs
+++ HashTable/Program.cs
@@ -5,13 +5,15 @@ namespace HashTable
     {
         static void Main(string[] args)
         {
-            MyMapNode<int, string> hash = new MyMapNode<int, string>(6);
-            hash.Add(0, "To");
-            hash.Add(1, "be");
-            hash.Add(2, "or");
-            hash.Add(3, "not");
-            hash.Add(4, "to");
-            hash.Add(5, "be");
+            string paragraph = "Paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations";
+            string[] array = paragraph.Split(" ");
+            MyMapNode<int, string> hash = new MyMapNode<int, string>(array.Length);
+            int key = 0;
+            foreach (string word in array)
+            {
+                hash.Add(key, word);
+                key++;
+            }
             Frequency count = new Frequency();
             count.FrequencyData(hash);
         }
